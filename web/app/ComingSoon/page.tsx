1afb235f20f80d1b176bef328ac995407b63634e
shubhiscoding <mygitcoint@gmail.com>
2025-01-08 01:01:03 +0530

    updatedd footer

--- web/app/ComingSoon/page.tsx
+++ web/app/ComingSoon/page.tsx
@@ -1,10 +1,11 @@
 "use client";
 import ComingSoonCard from "@/components/ComingSoon/coming-soon"
+import { Footer } from "@/components/footer";
 export default function Page(){
   return (
     <div style={{ display: 'flex',flexDirection: 'column', justifyContent: 'center', alignItems: 'center', height: "100%" }}>
       <ComingSoonCard />
-      <footer>Powered By Solana</footer>
+      <Footer />
     </div>
   )
 }
